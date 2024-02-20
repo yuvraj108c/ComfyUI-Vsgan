9d07f2c985c24fbb2aa1ce68fc5c1711843b2665
yuvraj <yuvraj.seegolam@umail.uom.ac.mu>
2024-02-20 23:51:49 +0400

    update model names + use models.json

--- nodes/upscale.py
+++ nodes/upscale.py
@@ -2,9 +2,13 @@ import os
 import folder_paths
 import subprocess
 import json
-from .utils import get_comfyui_basepath, get_customnode_basepath, get_config_path, get_video_metadata
+from .utils import get_comfyui_basepath, get_customnode_basepath, get_config_path, get_video_metadata, get_models_path, download_file
+
+models_dict = json.load(open(get_models_path()))
+models_dict = models_dict["upscaling"]
 
 ENGINE_DIR = os.path.join(get_comfyui_basepath(),"models/upscaler_trt_engines")
+ONNX_DIR = os.path.join(get_comfyui_basepath(),"models/onnx")
 
 class UpscaleVideoTrtNode:
     @classmethod
@@ -12,7 +16,7 @@ class UpscaleVideoTrtNode:
         return {
             "required": { 
                 "Filenames": ("VHS_FILENAMES",),
-                "engine": (os.listdir(ENGINE_DIR),),
+                "model": (list(models_dict.keys()),)
             }
         }
 
@@ -21,16 +25,31 @@ class UpscaleVideoTrtNode:
     CATEGORY = "Vsgan"
     OUTPUT_NODE=True
 
-    def main(self,Filenames,engine):
+    def main(self, Filenames, model):
         _, filenames = Filenames
         video_path = filenames[1]
-        engine_path = os.path.join(ENGINE_DIR,engine)
+
+        # check if engine exists
+        engine_path = os.path.join(ENGINE_DIR,f"{model}.engine")
+        if not os.path.exists(engine_path):
+            
+            # Download onnx if not exists
+            os.makedirs(ONNX_DIR, exist_ok=True)
+            onnx_path = os.path.join(ONNX_DIR,f"{model}.onnx")
+
+            if not os.path.exists(onnx_path):
+                download_file(models_dict[model], onnx_path )
+
+            # Create engine from onnx file
+            cmd = f"trtexec --fp16 --onnx={onnx_path} --minShapes=input:1x3x8x8 --optShapes=input:1x3x720x1280 --maxShapes=input:1x3x720x1280 --saveEngine={engine_path} --tacticSources=+CUDNN,-CUBLAS,-CUBLAS_LT --skipInference --infStreams=4 --builderOptimizationLevel=4"
+            subprocess.run(cmd,shell=True)
+
 
         # save config.json
         with open(get_config_path(), 'w') as f:
             json.dump({"video": video_path,"engine":engine_path}, f)
 
-        video_name = f"{engine.replace('.engine','')}_{os.path.basename(video_path)}"
+        video_name = f"{model}_{os.path.basename(video_path)}"
         output_path = os.path.join(os.path.join(get_comfyui_basepath(),"output"),video_name)
         subprocess.run(f"ffmpeg -f vapoursynth -i {os.path.join(get_customnode_basepath(),'inference.py')} {output_path} -y",shell=True)
                     
@@ -52,5 +71,5 @@ class UpscaleVideoTrtNode:
                 "resolution":f"{width} x {height}"
             }
         ] 
-        return {"ui": {"previews":previews,"data":data},}
+        return {"ui": {"previews": previews,"data":data},}
 
--- nodes/utils.py
+++ nodes/utils.py
@@ -1,5 +1,7 @@
 import os
 import imageio
+import httpx
+from tqdm import tqdm
 
 def get_video_metadata(video_path):
     reader = imageio.get_reader(video_path)
@@ -15,3 +17,22 @@ def get_customnode_basepath():
 
 def get_config_path():
     return os.path.join(get_customnode_basepath(),"config.json")
+
+def get_models_path():
+    return os.path.join(get_customnode_basepath(),"models.json")
+
+
+def download_file(url, save_path):
+    print(f"downloading {url} ...")
+    with httpx.stream("GET", url, follow_redirects=True) as stream:
+        total = int(stream.headers["Content-Length"])
+        with open(save_path, "wb") as f, tqdm(
+            total=total, unit_scale=True, unit_divisor=1024, unit="B"
+        ) as progress:
+            num_bytes_downloaded = stream.num_bytes_downloaded
+            for data in stream.iter_bytes():
+                f.write(data)
+                progress.update(
+                    stream.num_bytes_downloaded - num_bytes_downloaded
+                )
+                num_bytes_downloaded = stream.num_bytes_downloaded
