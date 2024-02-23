ed95051dc8e0ac8a5cdc3dbbd347c2546208e523
yuvraj <yuvraj.seegolam@umail.uom.ac.mu>
2024-02-23 21:57:12 +0400

    fix init

--- __init__.py
+++ __init__.py
@@ -1,11 +1,14 @@
 from .nodes.upscale import UpscaleVideoTrtNode
+from .nodes.depth_anything import DepthAnythingTrtNode
 
 NODE_CLASS_MAPPINGS = { 
     "UpscaleVideoTrtNode" : UpscaleVideoTrtNode,
+    "DepthAnythingTrtNode" : DepthAnythingTrtNode,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
      "UpscaleVideoTrtNode" : "Upscale Video Tensorrt", 
+     "DepthAnythingTrtNode" : "Depth Anything Tensorrt", 
 }
 
 WEB_DIRECTORY = "./web"
